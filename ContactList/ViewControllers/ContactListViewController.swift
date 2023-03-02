95c984b3a94cee872c785a523b428ca2b23ef012
Bektemur Mamashaev <bektemurmamashayev07@gmail.com>
2023-03-02 17:24:38 +0500

    Update ContactListViewController.swift

--- ContactList/ViewControllers/ContactListViewController.swift
+++ ContactList/ViewControllers/ContactListViewController.swift
@@ -28,11 +28,13 @@ class ContactListViewController: UITableViewController {
 
 // MARK: - TableViewDataSource
 extension ContactListViewController {
+   
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return contacts.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "contact", for: indexPath)
         var content = cell.defaultContentConfiguration()
         content.text = contacts[indexPath.row].contact
