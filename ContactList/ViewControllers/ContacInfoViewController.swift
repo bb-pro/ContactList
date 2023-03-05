69a534a435c96ff8cfead09cd6ace9d1cd01502e
Bektemur Mamashaev <bektemurmamashayev07@gmail.com>
2023-03-05 19:42:03 +0500

    Update ContacInfoViewController.swift

--- ContactList/ViewControllers/ContacInfoViewController.swift
+++ ContactList/ViewControllers/ContacInfoViewController.swift
@@ -12,12 +12,11 @@ class ContacInfoViewController: UIViewController {
     @IBOutlet var phoneLabel: UILabel!
     @IBOutlet var mailLabel: UILabel!
     
-    var phone: String!
-    var email: String!
+    var detail: Person!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        phoneLabel.text = phone
-        mailLabel.text = email
+        phoneLabel.text = detail.number
+        mailLabel.text = detail.email
     }
 }
